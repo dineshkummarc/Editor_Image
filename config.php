22fb6c10dc7f4615b33fd4450ff7b0baec190823
Raissadev <82960240+Raissadev@users.noreply.github.com>
2021-11-14 22:22:07 -0300

    Update file

--- config.php
+++ config.php
@@ -9,10 +9,10 @@
 
     spl_autoload_register($autoload);
     
-    define('BASE','http://localhost/Projects/myPhotoshop/');
+    define('BASE','http://localhost/Projects/');
     define('BASE_UPLOADS', __DIR__.'/uploads/');
 
-    if(!isset($_SESSION['name'])){
+    if(!isset($_SESSION['login'])){
         $_SESSION['name'] = 'Visitante';
         $_SESSION['email'] = 'email@example.com';
     }
